2104ff3362274484c7b6edfca807bf29c7d92e2a
Diego Combariza <Diego.combariza@uptc.edu.co>
2024-05-09 20:07:58 -0500

    fixing commit

--- index.js
+++ index.js
@@ -15,9 +15,8 @@ app.set('PORT', process.env.PORT || 3000);
 
 app.use(express.static('public/scripts'));
 
-app.get('/products', (req, res) => {
-    const data = JSON.parse(fs.readFileSync('./resources/data.json', 'utf-8'));
-    res.json(data);
+app.get('/checkout', function(req, res) {
+    res.render('checkout');
 });
 
 app.listen(app.get('PORT'), () => {
